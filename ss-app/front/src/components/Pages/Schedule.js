ad0d0e7bf5867ef89f7c3480f71979125cdfbc4e
Zoe Frongillo <z.frongillo@ufl.edu>
2021-02-04 17:57:00 -0500

    Schedule form 4 classes

--- ss-app/front/src/components/Pages/Schedule.js
+++ ss-app/front/src/components/Pages/Schedule.js
@@ -6,14 +6,54 @@ import './basic.css';
 const Schedule = props => {
 
   //internal change functions
-  const CourseNumInt = event => {
-    console.log('CourseNumInt Triggered');
+  const SemInt = event => {
+    console.log('SemIntTriggered');
 
     //change by calling App external function
-    props.C_NumUpdate(event.target.value);
+    props.CL_NumUpdate(event.target.value);
 
     //log
-    console.log('C_Num: ', event.target.value);
+    console.log('SemInt: ', event.target.value);
+  };
+
+  const CourseNumInt1 = event => {
+    console.log('CourseNumInt1 Triggered');
+
+    //change by calling App external function
+    props.C_NumUpdate1(event.target.value);
+
+    //log
+    console.log('C_Num1: ', event.target.value);
+  };
+
+  const CourseNumInt2 = event => {
+    console.log('CourseNumInt2 Triggered');
+
+    //change by calling App external function
+    props.C_NumUpdate2(event.target.value);
+
+    //log
+    console.log('C_Num2: ', event.target.value);
+  };
+
+  const CourseNumInt3 = event => {
+    console.log('CourseNumInt3 Triggered');
+
+    //change by calling App external function
+    props.C_NumUpdate3(event.target.value);
+
+    //log
+    console.log('C_Num3: ', event.target.value);
+  };
+
+  const CourseNumInt4 = event => {
+    console.log('CourseNumInt4 Triggered');
+
+    //change by calling App external function
+    props.C_NumUpdate4(event.target.value);
+
+    //log
+    console.log('C_Num4: ', event.target.value);
   };
 
   const ClassNumInt = event => {
@@ -32,26 +72,75 @@ const Schedule = props => {
       
       <Container>
         <Row>
-          <Col sm={4}>Filter Menu
+          <Col sm={4}>
 
             <Card body>
 
             <div className='col-12'>
                 <Form onSubmit={props.check}>
-                  <h1>Search</h1>
-            
+                  <h2>Add Courses</h2>
 
+                  
                   <div className='h1_p_1'>
                     <Form.Row>
-                      <Form.Label>Course Number</Form.Label>
+                      <Form.Label>Semester</Form.Label>
+                      <select className="form-control" name="Semester" onChange={ClassNumInt} required="true">
+                          <option selected>Semester - Required</option>
+                          <option value="f21">Fall 2021</option>
+                          <option value="s21">Spring 2021</option>
+                          <option value="f20">Fall 2020</option>
+                      </select>
+                    </Form.Row>
+                  </div>
+            
+                  <div className='h1_p_1'>
+                    <Form.Row>
+                      <Form.Label>Course Number 1</Form.Label>
                       <Form.Control
                         input
                         placeholder='ex. ACG2021'
-                        id='CourseNum'
-                        onChange={CourseNumInt}
+                        id='CourseNum1'
+                        onChange={CourseNumInt1}
                       />
                     </Form.Row>
                   </div>
+
+                  <div className='h1_p_1'>
+                    <Form.Row>
+                      <Form.Label>Course Number 2</Form.Label>
+                      <Form.Control
+                        input
+                        placeholder='ex. ACG2021'
+                        id='CourseNum2'
+                        onChange={CourseNumInt2}
+                      />
+                    </Form.Row>
+                  </div>
+
+                  <div className='h1_p_1'>
+                    <Form.Row>
+                      <Form.Label>Course Number 3</Form.Label>
+                      <Form.Control
+                        input
+                        placeholder='ex. ACG2021'
+                        id='CourseNum3'
+                        onChange={CourseNumInt3}
+                      />
+                    </Form.Row>
+                  </div>
+
+                  <div className='h1_p_1'>
+                    <Form.Row>
+                      <Form.Label>Course Number 4</Form.Label>
+                      <Form.Control
+                        input
+                        placeholder='ex. ACG2021'
+                        id='CourseNum4'
+                        onChange={CourseNumInt4}
+                      />
+                    </Form.Row>
+                  </div>
+
 
                   <div className='h1_p_1'>
                     <Form.Row>
@@ -65,8 +154,8 @@ const Schedule = props => {
                     </Form.Row>
                   </div>
 
-          
 
+          
 
                   <div className='h1_p'>
                     <Button variant='primary' type='submit'>

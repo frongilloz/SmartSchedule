baec91fc07f187eb53dc4e22f8692f1b1a4c4e67
dsantonil <dsantonil@ufl.edu>
2021-03-12 21:25:56 -0500

    current changes to schedule generate

--- ss-app/front/src/components/pages.js
+++ ss-app/front/src/components/pages.js
@@ -101,7 +101,8 @@ export const Schedule = () => {
 
     const periods = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "E1", "E2", "E3"];
     const daysShort = ["M", "T", "W", "R", "F"]
-    const finalSchedules = []
+    let finalSchedules = []
+    console.log('what is finalSchedules doing?.....', finalSchedules)
     let checkArray = [1]
     console.log('Before: ', scheduleArray)
     //console.log(checkArray.length)
@@ -109,6 +110,7 @@ export const Schedule = () => {
     let day_index = 0;
     let period_index1 = 0;
     let period_index2 = 0;
+    let totalScheduleCount = 1;
 
     if (Array.isArray(responseData) && checkArray.length && responseData[0]) {  //check if responseData array exists and if something exists in it (try taking out the checkArray thing)
       console.log("Wow, something's here")
@@ -118,43 +120,67 @@ export const Schedule = () => {
       responseDataLength = responseData
       console.log('length of responseData is: ', responseDataLength.length)
 
+      //loop to get the total combination of schedules
+      for (let a = 0; a < responseDataLength.length; a++) {
+        let sectionsAmount = responseData[a].sections
+        //console.log('sectionsAmount is :', sectionsAmount.length)
+        totalScheduleCount = totalScheduleCount * sectionsAmount.length
+        //console.log('current totalScheduleCount is :', totalScheduleCount)
+      }
 
+      console.log('total combinations of schedules is: ', totalScheduleCount)
+
+      //fill the final schedule array with empty arrays 
+      for (let c = 0; c < totalScheduleCount; c++) {
+        let emptySchedule = Array(14).fill(0).map(row => new Array(6).fill(" "))
+        finalSchedules.push(emptySchedule)
+      }
+
+      console.log('finalSchedules initial status: ', finalSchedules)
+
+      let count = 0
       for (let k = 0; k < responseDataLength.length; k++) {
 
       let sectionsArray = responseData[k].sections
       console.log('sectionsArray is: ', sectionsArray)
 
-      for (let i = 0; i < sectionsArray.length; i++) {
-        let meetTArray = sectionsArray[i].meetTimes
-        console.log('meetTArray is: ', meetTArray)
-        let newSchedule = Array(14).fill(0).map(row => new Array(6).fill(" "))
+        //while (count != totalScheduleCount) {
+          for (let i = 0; i < sectionsArray.length; i++) {
+            let meetTArray = sectionsArray[i].meetTimes
+            //console.log('meetTArray is: ', meetTArray)
+            for (let j = 0; j < meetTArray.length; j++) {
 
-        for (let j = 0; j < meetTArray.length; j++) {
-          //console.log('meetTDay is: ', meetTArray[j].meetDays[0])
-          //console.log(meetTArray[j].meetPeriodBegin)
-          //console.log(meetTArray[j].meetPeriodEnd)
+              day_index = daysShort.indexOf(meetTArray[j].meetDays[0])
+              console.log('day_index is: ', day_index)
 
-          day_index = daysShort.indexOf(meetTArray[j].meetDays[0])
-          console.log('day_index is: ', day_index)
+              period_index1 = periods.indexOf(meetTArray[j].meetPeriodBegin)
+              console.log('period_index1 is: ', period_index1)
 
-          period_index1 = periods.indexOf(meetTArray[j].meetPeriodBegin)
-          console.log('period_index1 is: ', period_index1)
+              period_index2 = periods.indexOf(meetTArray[j].meetPeriodEnd)
+              console.log('period_index2 is: ', period_index2)
 
-          period_index2 = periods.indexOf(meetTArray[j].meetPeriodEnd)
-          console.log('period_index2 is: ', period_index2)
+              let newSchedule = finalSchedules[count]
+              console.log('newSchedule is: ', newSchedule)
 
-          if (period_index1 === period_index2)
-            newSchedule[period_index1][day_index] = responseData[k].code
-          else {
-            newSchedule[period_index1][day_index] = responseData[k].code
-            newSchedule[period_index2][day_index] = responseData[k].code
-          }        
+              if (period_index1 === period_index2)
+                newSchedule[period_index1][day_index] = responseData[k].code
+              else {
+                newSchedule[period_index1][day_index] = responseData[k].code
+                newSchedule[period_index2][day_index] = responseData[k].code
+              }
+              
+            }
 
-        }
+            console.log('what should new schedule look like: ', newSchedule)
 
-        console.log('newSchedule is : ', newSchedule)
-        finalSchedules.push(newSchedule) 
-      }
+            for (let b = 0; b < totalScheduleCount / sectionsArray.length; b++) {
+              finalSchedules[count] = newSchedule
+              count++
+              if (count === totalScheduleCount) count = 0
+            }
+            console.log('finalSchedules is : ', finalSchedules)
+          }
+        //}
       }
     }
     else {
@@ -164,7 +190,7 @@ export const Schedule = () => {
     }
 
     setScheduleArray(finalSchedules)
-    console.log('After: ', finalSchedules)
+    console.log('After: ', scheduleArray)
 
   };
 

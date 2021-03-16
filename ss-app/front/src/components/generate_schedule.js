1e8d4b44fde5ce2791345d6f5e0af532b1df3e15
dsantonil <dsantonil@ufl.edu>
2021-03-15 21:36:11 -0400

    updating generate_schedule.js

--- ss-app/front/src/components/generate_schedule.js
+++ ss-app/front/src/components/generate_schedule.js
@@ -1,98 +1,108 @@
 const periods = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "E1", "E2", "E3"];
 const daysShort = ["M", "T", "W", "R", "F"]
 
-
-const times = [
-    "7:25 AM - 8:15 AM", "8:30 AM - 9:20 AM", "9:35 AM - 10:25 AM",
-    "10:40 AM - 11:30 AM", "11:45 AM - 12:35 PM", "12:50 PM - 1:40 PM",
-    "1:55 PM - 2:45 PM", "3:00 PM - 3:50 PM", "4:05 PM - 4:55 PM",
-    "5:10 PM - 6:00 PM", "6:15 PM - 7:05 PM", "7:20 PM - 8:10 PM",
-    "8:20 PM - 9:10 PM", "9:20 PM - 10:10 PM"];
-
-// maybe have a 2D array that will contain the in-class potential schedules 
-// first, fill it with empty space
-var TestSchdule = Array(14).fill(0).map(row => new Array(6).fill(" "))
-
-
 export function generateSchedule(responseData) {
 
-    let checkArray = [1] 
-    let oldSchedule = Array(14).fill(0).map(row => new Array(6).fill(" "))
-    let newSchedule = Array(14).fill(0).map(row => new Array(6).fill(" "))
-    oldSchedule = TestSchdule;
+  let finalSchedules = []
+  console.log('what is finalSchedules doing?.....', finalSchedules)
+  let checkArray = [1]
+  //console.log('Before: ', scheduleArray)
+  //console.log(checkArray.length)
 
-    let listSchedules  = [oldSchedule, oldSchedule]
+  let day_index = 0;
+  let period_index1 = 0;
+  let period_index2 = 0;
+  let totalScheduleCount = 1;
 
-    //console.log(checkArray.length)
-    console.log("Resp data in gen_schedule")
-    console.log(responseData)
+  let newSchedule = []
+  let dummySchedule = Array(14).fill(0).map(row => new Array(6).fill(" "))
 
-    let day_index = 0;
-    let period_index1 = 0;
-    let period_index2 = 0;
+  if (Array.isArray(responseData) && checkArray.length && responseData[0]) {  //check if responseData array exists and if something exists in it (try taking out the checkArray thing)
+    console.log("Wow, something's here")
+    console.log('ResponseData (from schedule.js) is: ', responseData)
 
-    console.log('1) length of resposneData is: ', (responseData.length) )
+    let responseDataLength = [];
+    responseDataLength = responseData
+    console.log('length of responseData is: ', responseDataLength.length)
 
-    if (Array.isArray(responseData) && checkArray.length && responseData[0]) {
-      console.log("Wow, something's here")
-      console.log('ResponseData (from pages.js) is: ', responseData)
-
-      let responseDataLength = [];
-      responseDataLength = responseData
-      console.log('length of resposneData is: ', responseData.length)
-
-
-      for (let k = 0; k < responseDataLength.length; k++) {
-
-        let sectionsArray = responseData[k].sections
-        //console.log('sectionsArray is: ', sectionsArray)
-        let meetTArray = sectionsArray[0].meetTimes
-        //console.log('meetTArray is: ', meetTArray)
-
-        for (let j = 0; j < meetTArray.length; j++) {
-          //console.log('meetTDay is: ', meetTArray[j].meetDays[0])
-          //console.log(meetTArray[j].meetPeriodBegin)
-          //console.log(meetTArray[j].meetPeriodEnd)
-
-          day_index = daysShort.indexOf(meetTArray[j].meetDays[0])
-          //console.log('day_index is: ', day_index)
-
-          period_index1 = periods.indexOf(meetTArray[j].meetPeriodBegin)
-          //console.log('period_index1 is: ', period_index1)
-
-          period_index2 = periods.indexOf(meetTArray[j].meetPeriodEnd)
-          //console.log('period_index2 is: ', period_index2)
-
-          if (period_index1 === period_index2)
-            TestSchdule[period_index1][day_index] = responseData[k].code
-          else {
-            TestSchdule[period_index1][day_index] = responseData[k].code
-            TestSchdule[period_index2][day_index] = responseData[k].code
-            }
-          }
-        }
-
-        console.log('Before: ', TestSchdule)
-        console.log('oldSchedule is : ', oldSchedule)
-        console.log('newSchedule is : ', newSchedule)
-
-
-        // To be changed: placeholder for returning multiple schedules
-        // List of schedules
-        listSchedules  = [TestSchdule, oldSchedule]
-        console.log('listSchedules: ', listSchedules)
-        
-      }
-    else {
-      console.log("I guess there's nothing here")
-      console.log(responseData)
-      //console.log('Is there really nothing here?...', responseData[0].code)
+    //loop to get the total combination of schedules
+    for (let a = 0; a < responseDataLength.length; a++) {
+      let sectionsAmount = responseData[a].sections
+      //console.log('sectionsAmount is :', sectionsAmount.length)
+      totalScheduleCount = totalScheduleCount * sectionsAmount.length
+      //console.log('current totalScheduleCount is :', totalScheduleCount)
     }
 
-    console.log('After TestSchdule: ', TestSchdule)
-    console.log('After listSchedules: ', listSchedules)
+    console.log('total combinations of schedules is: ', totalScheduleCount)
 
-    // Return the multiple schedules
-    return listSchedules
+    //fill the final schedule array with empty arrays 
+    for (let c = 0; c < totalScheduleCount; c++) {
+      let emptySchedule = Array(14).fill(0).map(row => new Array(6).fill(" "))
+      finalSchedules.push(emptySchedule)
+    }
 
+    //console.log('finalSchedules initial status: ', finalSchedules)
+
+    let count = 0
+    for (let k = 0; k < responseDataLength.length; k++) {
+
+      let sectionsArray = responseData[k].sections
+      //console.log('sectionsArray is: ', sectionsArray)
+      for (let b = 0; b < totalScheduleCount / sectionsArray.length; b++) {
+
+        for (let i = 0; i < sectionsArray.length; i++) {
+          let meetTArray = sectionsArray[i].meetTimes
+          //console.log('meetTArray is: ', meetTArray)
+          for (let j = 0; j < meetTArray.length; j++) {
+            day_index = daysShort.indexOf(meetTArray[j].meetDays[0])
+            //console.log('day_index is: ', day_index)
+
+            period_index1 = periods.indexOf(meetTArray[j].meetPeriodBegin)
+            //console.log('period_index1 is: ', period_index1)
+
+            period_index2 = periods.indexOf(meetTArray[j].meetPeriodEnd)
+            //console.log('period_index2 is: ', period_index2)
+
+            newSchedule = finalSchedules[count]
+            dummySchedule = Array(14).fill(0).map(row => new Array(6).fill(" "))
+
+            //console.log('newSchedule is: ', newSchedule)
+
+            if (period_index1 === period_index2) {
+              if (newSchedule[period_index1][day_index] != " ")
+                console.log("uh oh, there are two classes that could be here")
+              newSchedule[period_index1][day_index] = responseData[k].code
+              dummySchedule[period_index1][day_index] = responseData[k].code
+            }
+            else {
+              if (newSchedule[period_index1][day_index] != " " || newSchedule[period_index2][day_index] != " ")
+                console.log("uh oh, there are two classes that could be here")
+              newSchedule[period_index1][day_index] = responseData[k].code
+              newSchedule[period_index2][day_index] = responseData[k].code
+              dummySchedule[period_index1][day_index] = responseData[k].code
+              dummySchedule[period_index2][day_index] = responseData[k].code
+            }
+
+          }
+
+          //console.log('what should be added in this itteration: ', dummySchedule)
+          //console.log('what does new schedule look like: ', newSchedule)
+
+          finalSchedules[count] = newSchedule
+          count++
+
+        }
+        if (count === totalScheduleCount) count = 0
+        console.log('finalSchedules is : ', finalSchedules)
+      }
+
+    }
+  }
+  else {
+    console.log("I guess there's nothing here")
+    console.log(responseData)
+    //console.log('Is there really nothing here?...', props.responseData[0].code)
+  }
+
+  return finalSchedules
 }

5b6d66d8149eeb8571bbe3a0de6f64468f46573f
Eric Roque <eric4707@ufl.edu>
2021-03-26 11:46:12 -0400

    Dialogflow backend connetion init

---
+++ ss-app/back/controllers/dialogflow.js
@@ -0,0 +1,71 @@
+import express from 'express';
+import dialogflow from 'dialogflow';
+import config from '../config/keys.js'
+
+const router = express.Router();
+
+const projectId = config.googleProjectID;
+const sessionId = config.dialogFlowSessionID;
+const languageCode = config.dialogFlowSessionLanguageCode;
+
+
+// Create a new session
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+
+//Text Query Route
+router.post('/textQuery', async (req, res) => {
+ 
+    // The text query request.
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                // The query to send to the dialogflow agent
+                text: req.body.text,
+                // The language used by the client (en-US)
+                languageCode: languageCode,
+            },
+        },
+    };
+
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+
+    res.send(result);
+      
+});
+
+//Event Query Route
+router.post('/eventQuery', async (req, res) => {
+ 
+    // The text query request.
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            event: {
+                // The query to send to the dialogflow agent
+                name: req.body.event,
+                // The language used by the client (en-US)
+                languageCode: languageCode,
+            },
+        },
+    };
+
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+
+    res.send(result);
+      
+});
+
+export default router;

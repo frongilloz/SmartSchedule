51891be72dd1f555dcdbc5de6a825f9669d27a16
scomora <spencer.comora@gmail.com>
2021-04-19 13:34:11 -0400

    correct logic, but outermost loop not quite working

--- ss-app/front/src/components/get_mapping_distance.js
+++ ss-app/front/src/components/get_mapping_distance.js
@@ -40,198 +40,185 @@ export function get_mapping_distance(final_schedule_info, testSc) {
     let schedule;
     let scheduleIdx;
 
+    
     for (scheduleIdx = 0; scheduleIdx < testSc.length; scheduleIdx++) {
-    schedule = testSc[scheduleIdx];
+        console.log(`scheduleIdx = ${scheduleIdx}`);
+        schedule = testSc[scheduleIdx];
 
-    for (dayIdx = 0; dayIdx < schedule[0].length; dayIdx++) {
-        for (hourIdx = 0; hourIdx < (schedule.length - 1); hourIdx++) {
-        earlierScheduleEntry = schedule[hourIdx][dayIdx];
-        laterScheduleEntry = schedule[hourIdx + 1][dayIdx];
+        for (dayIdx = 0; dayIdx < schedule[0].length; dayIdx++) {
+            for (hourIdx = 0; hourIdx < (schedule.length - 1); hourIdx++) {
+                earlierScheduleEntry = schedule[hourIdx][dayIdx];
+                laterScheduleEntry = schedule[hourIdx + 1][dayIdx];
 
-        if (earlierScheduleEntry === " ") {
-            continue;
-        }
-
-        if (laterScheduleEntry === " ") {
-            continue;
-        }
-
-        if (earlierScheduleEntry === laterScheduleEntry) {
-            continue;
-        }
-
-        /* get the iterators associated with these courses */
-        earlierScheduleIterator = getIterator(earlierScheduleEntry);
-        laterScheduleIterator = getIterator(laterScheduleEntry);
-
-        /* based on the count value, get the course info associated with them */
-        earlierCourseInfo = final_schedule_info[earlierScheduleIterator.offsetIntoFinalScheduleInfo][scheduleIdx];
-        laterCourseInfo = final_schedule_info[laterScheduleIterator.offsetIntoFinalScheduleInfo][scheduleIdx];
-
-        // log 
-        //console.log("earlierCourseInfo", earlierCourseInfo)
-        //console.log("laterCourseInfo", laterCourseInfo)
-
-        fromCode = "NEB";
-        toCode = "GER";
-
-        /* @TODO: extract building code of relevant sections.
-            if they're both in-person, call getWalkingDistance() */
-
-        let currover15=false;
-        let currIsOnline= false;
-
-        if (earlierCourseInfo.section_web === "AD") {
-            continue;
-        }
-
-        if (laterCourseInfo.section_web === "AD") {
-            continue;
-        }
-
-        // NEED to change; RN getting the building location from the FIRST building, not the actual one
-
-        const DAY_MAP = [
-            "M",
-            "T",
-            "W",
-            "R",
-            "F"
-        ];
-
-        let meetDay;
-        let meetTime;
-
-        let earlierCourseSection;
-        let laterCourseSection;
-
-        let foundEarlier = false;
-        let foundLater = false;
-
-        let idx;
-
-        const earlierMeetTimes = earlierCourseInfo.section_mT;
-        const laterMeetTimes = laterCourseInfo.section_mT;
-
-        let meetDayIdx;
-
-        for (idx = 0; idx < earlierMeetTimes.length; idx++) {
-            meetTime = earlierMeetTimes[idx];
-
-            meetDayIdx = meetTime.meetDays.findIndex(dayChar => dayChar === DAY_MAP[dayIdx]);
-
-            if (meetDayIdx !== -1) {
-                meetDay = meetTime;
-                if (parseInt(meetDay.meetPeriodBegin) === hourIdx) {
-                    foundEarlier = true;
-                    earlierCourseSection = meetTime;
+                if (earlierScheduleEntry === " ") {
+                    continue;
                 }
+
+                if (laterScheduleEntry === " ") {
+                    continue;
+                }
+
+                if (earlierScheduleEntry === laterScheduleEntry) {
+                    continue;
+                }
+
+                /* get the iterators associated with these courses */
+                earlierScheduleIterator = getIterator(earlierScheduleEntry);
+                laterScheduleIterator = getIterator(laterScheduleEntry);
+
+                /* based on the count value, get the course info associated with them */
+                earlierCourseInfo = final_schedule_info[earlierScheduleIterator.offsetIntoFinalScheduleInfo][scheduleIdx];
+                laterCourseInfo = final_schedule_info[laterScheduleIterator.offsetIntoFinalScheduleInfo][scheduleIdx];
+
+                // log 
+                //console.log("earlierCourseInfo", earlierCourseInfo)
+                //console.log("laterCourseInfo", laterCourseInfo)
+
+                fromCode = "NEB";
+                toCode = "GER";
+
+                /* @TODO: extract building code of relevant sections.
+                    if they're both in-person, call getWalkingDistance() */
+
+                let currover15=false;
+                let currIsOnline= false;
+
+                if (earlierCourseInfo.section_web === "AD") {
+                    continue;
+                }
+
+                if (laterCourseInfo.section_web === "AD") {
+                    continue;
+                }
+
+                // NEED to change; RN getting the building location from the FIRST building, not the actual one
+
+                const DAY_MAP = [
+                    "M",
+                    "T",
+                    "W",
+                    "R",
+                    "F"
+                ];
+
+                let meetDay;
+                let meetTime;
+
+                let earlierCourseSection;
+                let laterCourseSection;
+
+                let foundEarlier = false;
+                let foundLater = false;
+
+                let idx;
+
+                const earlierMeetTimes = earlierCourseInfo.section_mT;
+                const laterMeetTimes = laterCourseInfo.section_mT;
+
+                let meetDayIdx;
+                let meetDayChar;
+
+                for (idx = 0; idx < earlierMeetTimes.length; idx++) {
+                    meetTime = earlierMeetTimes[idx];
+
+                    meetDayChar = meetTime.meetDays.find(dayChar => dayChar === DAY_MAP[dayIdx]);
+
+                    if (meetDayChar) {
+                        meetDay = meetTime;
+                        if (parseInt(meetDay.meetPeriodBegin) === hourIdx) {
+                            foundEarlier = true;
+                            earlierCourseSection = meetTime;
+                            break;
+                        }
+                    }
+                }
+
+                if (!foundEarlier) {
+                    return [];
+                }
+
+                let newHourIdx = hourIdx + (parseInt(earlierCourseSection.meetPeriodEnd) - parseInt(earlierCourseSection.meetPeriodBegin)) + 1;
+
+                for (idx = 0; idx < laterMeetTimes.length; idx++) {
+                    meetTime = laterMeetTimes[idx];
+
+                    meetDayChar = meetTime.meetDays.find(dayChar => dayChar === DAY_MAP[dayIdx]);
+
+                    if (meetDayChar) {
+                        meetDay = meetTime;
+                        if (parseInt(meetDay.meetPeriodBegin) === newHourIdx) {
+                            foundLater = true;
+                            laterCourseSection = meetTime;
+                            break;
+                        }
+                    }
+                }
+
+                if (!foundLater) {
+                    return [];
+                }
+                    
+                fromCode = earlierCourseSection.meetBuilding;
+                toCode = laterCourseSection.meetBuilding;
+
+                walkingDurationMins = getWalkingDurationBetweenMins(fromCode, toCode);
+
+                //console.log(`${walkingDurationMins} minutes`);
+
+                // If the duration is greater than 15 minutes
+                if (walkingDurationMins > 15) {
+                    console.log(`Can't make it in time from ${fromCode} to ${toCode}!!!`);
+                    currover15=true;
+                }
+
+                // Later
+                let dayHourPair = {
+                    scheduleIdx:  scheduleIdx,
+                    dayIdx:       dayIdx,
+                    hourIdx:      hourIdx
+                };
+                conflictingDayHourPairs.push(dayHourPair);
+
+                //////
+                let currDay;
+                if(dayIdx ===  0){
+                    currDay = 'M';
+                }
+                else if(dayIdx ===  1){
+                    currDay = 'T';
+                }
+                else if(dayIdx === 2){
+                    currDay = 'W';
+                }
+                else if(dayIdx === 3){
+                    currDay = 'Th';
+                }
+                else if(dayIdx === 4){
+                    currDay = 'F';
+                }
+                else if(dayIdx === 5){
+                    currDay = 'Sat';
+                }
+
+
+                // Object to push
+                let wlk_object={
+                    scheduleIdx: scheduleIdx,
+                    over15: currover15,
+                    loc1: fromCode,
+                    loc2: toCode,
+                    duration: walkingDurationMins.toFixed(3),
+                    dayIdx: currDay,
+                    hourIdx: hourIdx+1,
+                    class1: earlierCourseInfo.course_code,
+                    class2:laterCourseInfo.course_code,
+                    isOnline: currIsOnline 
+                }
+
+                // front end object
+                walking_Durs.push(wlk_object)
             }
         }
-
-        for (idx = 0; idx < laterMeetTimes.length; idx++) {
-            meetTime = laterMeetTimes[idx];
-
-            meetDayIdx = meetTime.meetDays.findIndex(dayChar => dayChar === DAY_MAP[dayIdx]);
-
-            if (meetDayIdx !== -1) {
-                meetDay = meetTime;
-                if (parseInt(meetDay.meetPeriodBegin) === hourIdx) {
-                    foundLater = true;
-                    laterCourseSection = meetTime;
-                }
-            }
-        }
-
-        /* for each section meet time */
-        // earlierCourseInfo.section_mT.forEach(meetTime => {
-        //     meetDay = meetTime.meetDays.find(DAY_MAP[dayIdx])
-
-        //     if (meetDay) {
-        //         if (meetDay.meetPeriodBegin.parseInt() === hourIdx) {
-        //             foundEarlier = true;
-        //             earlierCourseSection = meetTime;
-        //         }
-        //     }
-        // })
-
-        // laterCourseInfo.section_mT.forEach((meetTime) => {
-        //     meetDay = meetTime.meetDays.find(DAY_MAP[dayIdx])
-
-        //     if (meetDay) {
-        //         if (meetDay.meetPeriodBegin.parseInt() === hourIdx) {
-        //             foundLater = true;
-        //             laterCourseSection = meetTime;
-        //         }
-        //     }
-        // })
-
-        if (!(foundEarlier && foundLater)) {
-            return [];
-        }
-            
-            /*  */
-        fromCode = earlierCourseSection.meetBuilding;
-        toCode = laterCourseSection.meetBuilding;
-
-        walkingDurationMins = getWalkingDurationBetweenMins(fromCode, toCode);
-
-        //console.log(`${walkingDurationMins} minutes`);
-
-        // If the duration is greater than 15 minutes
-        if (walkingDurationMins > 15) {
-            console.log(`Can't make it in time from ${fromCode} to ${toCode}!!!`);
-            currover15=true;
-        }
-
-        // Later
-        let dayHourPair = {
-            scheduleIdx:  scheduleIdx,
-            dayIdx:       dayIdx,
-            hourIdx:      hourIdx
-        };
-        conflictingDayHourPairs.push(dayHourPair);
-
-        //////
-        let currDay;
-        if(dayIdx ===  0){
-            currDay = 'M';
-        }
-        else if(dayIdx ===  1){
-            currDay = 'T';
-        }
-        else if(dayIdx === 2){
-            currDay = 'W';
-        }
-        else if(dayIdx === 3){
-            currDay = 'Th';
-        }
-        else if(dayIdx === 4){
-            currDay = 'F';
-        }
-        else if(dayIdx === 5){
-            currDay = 'Sat';
-        }
-
-
-        // Object to push
-        let wlk_object={
-            scheduleIdx: scheduleIdx,
-            over15: currover15,
-            loc1: fromCode,
-            loc2: toCode,
-            duration: walkingDurationMins.toFixed(3),
-            dayIdx: currDay,
-            hourIdx: hourIdx+1,
-            class1: earlierCourseInfo.course_code,
-            class2:laterCourseInfo.course_code,
-            isOnline: currIsOnline 
-        }
-
-        // front end object
-        walking_Durs.push(wlk_object)
-        }
-    }
     }
     
     console.log("conflictingDayHourPairs", conflictingDayHourPairs);
